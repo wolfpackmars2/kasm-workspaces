b6ef0a47edcc3df0ca5d4d6e3e0c9570fb5dc5ae
S Groesz <steve@groesz.org>
2026-01-02 21:36:11 -0600

    refactor repo for more than kasm

---
+++ kasm-workspaces/src/ubuntu/install/solvespace/install_solvespace.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -ex
+
+# Install Solvespace
+apt update
+apt install -y git build-essential cmake zlib1g-dev libpng-dev \
+    libcairo2-dev libfreetype6-dev libjson-c-dev \
+    libfontconfig1-dev libgtkmm-3.0-dev libpangomm-1.4-dev \
+    libgl-dev libglu-dev libspnav-dev qt6-base-dev
+echo "Cloning solvespace source..."
+git clone https://github.com/solvespace/solvespace
+cd solvespace
+git submodule update --init # Prepare submodules
+mkdir build
+cd build
+echo "Building solvespace"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DENABLE_OPENMP=ON -DENABLE_LTO=ON -DUSE_QT_GUI=ON
+make
+make install
+echo "Cleaning up..."
+cd ../..
+rm -rf solvespace
+echo "Done."
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
